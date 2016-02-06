0c1416cd283cbf9f18d44a9aea77d778cf86c610
msommerx <martin.sommer@hs-rm.de>
2016-02-06 12:39:40 +0100

    b

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ udevd &
 modprobe i2c-bcm2708
 modprobe i2c-dev
 
-sudo chmod a+rw /dev/hidraw4
-python /app/monitor.py /dev/hidraw4
+sudo chmod a+rw /dev/hidraw2
+python /app/monitor.py /dev/hidraw2
