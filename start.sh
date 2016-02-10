9f93bbf969b1d36cf5ee893c9b57e2ee7f0ec01b
msommerx <martin.sommer@hs-rm.de>
2016-02-10 10:51:07 +0100

    b

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo chmod a+rw /dev/hidraw0
-python /app/monitor.py /dev/hidraw0
-
 sudo modprobe i2c-dev
 sudo i2cdetect -y 1
+sudo chmod a+rw /dev/hidraw0
+python /app/monitor.py /dev/hidraw0
